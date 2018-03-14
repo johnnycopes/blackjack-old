bb9dff2370b389477c593958e5634600b5d236b2
John Coppola <jcoppola@instien.com>
2018-03-14 19:52:21 -0400

    beefs up webpack config and npm scripts, prepares for conversion to TS

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import Game from './components/game';
+import Game from './modules/game';
 
 new Game;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,139 @@
-const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const extractSass = new ExtractTextPlugin({
-  filename: "[name].[contenthash].css",
-  disable: process.env.NODE_ENV === "development"
-});
+// const path = require("path");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const extractSass = new ExtractTextPlugin({
+//   filename: "[name].[contenthash].css",
+//   disable: process.env.NODE_ENV === "development"
+// });
 
-module.exports = {
-  entry: "./src/js/index.js",
-  devtool: 'source-map',
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-        }, {
-          loader: "css-loader", // translates CSS into CommonJS
-          options: {
-            sourceMap: true
-          }
-        }, {
-          loader: "sass-loader", // compiles Sass to CSS
-          options: {
-            sourceMap: true
-          }
-        }]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader?name=[name].[ext]&outputPath=img/'
-        ]
-      },
-      //  for production only
-      // {
-      //   test: /\.scss$/,
-      //   use: extractSass.extract({
-      //           use: [{
-      //               loader: "css-loader"
-      //           }, {
-      //               loader: "sass-loader"
-      //           }],
-      //           // use style-loader in development
-      //           fallback: "style-loader"
-      //       })
-      // }
-    ]
-  },
-  // plugins: [
-  //   extractSass
-  // ]
+// module.exports = {
+//   entry: "./src/js/index.js",
+//   devtool: 'source-map',
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname, 'dist')
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["env"]
+//           }
+//         }
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: [{
+//           loader: "style-loader" // creates style nodes from JS strings
+//         }, {
+//           loader: "css-loader", // translates CSS into CommonJS
+//           options: {
+//             sourceMap: true
+//           }
+//         }, {
+//           loader: "sass-loader", // compiles Sass to CSS
+//           options: {
+//             sourceMap: true
+//           }
+//         }]
+//       },
+//       {
+//         test: /\.(png|svg|jpg|gif)$/,
+//         use: [
+//           'file-loader?name=[name].[ext]&outputPath=img/'
+//         ]
+//       },
+//       //  for production only
+//       // {
+//       //   test: /\.scss$/,
+//       //   use: extractSass.extract({
+//       //           use: [{
+//       //               loader: "css-loader"
+//       //           }, {
+//       //               loader: "sass-loader"
+//       //           }],
+//       //           // use style-loader in development
+//       //           fallback: "style-loader"
+//       //       })
+//       // }
+//     ]
+//   },
+//   // plugins: [
+//   //   extractSass
+//   // ]
+// };
+
+
+
+
+
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
+
+let config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, './public'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/, // files ending with .ts
+				exclude: /node_modules/, // exclude the node modules directory
+				use: 'ts-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["env"]
+					}
+				}
+			},
+			{
+				test: /\.scss$/, // files ending with .scss
+				use: ['css-hot-loader'].concat(
+					ExtractTextWebpackPlugin.extract({
+						fallback: 'style-loader',
+						use: ['css-loader', 'sass-loader']
+					})
+				)
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/, // for other file types
+				use: ['file-loader?name=[name].[ext]&outputPath=img/']
+			}
+		] // end rules
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	plugins: [
+		new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin and name our CSS file
+	],
+	devServer: {
+		contentBase: path.resolve(__dirname, './dist'), // A directory or URL to serve HTML content from
+		historyApiFallback: true, // fallback to /index.html for SPA
+		inline: true,
+		open: true // open default browser on launch
+	},
+	devtool: 'eval-source-map' // for choosing a style of source map
 };
+
+module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin(), // call the uglify plugin
+		new OptimizeCSSAssets() // call the CSS optimizer (for minification)
+	);
+}
